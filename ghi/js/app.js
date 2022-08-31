a76fa27f8c55e99459f092bdb10e8681350ce2ae
Qingying Meng <qmeng222@gmail.com>
2022-08-31 01:52:09 -0700

    start/end dates added

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,10 +1,13 @@
-function createCard(name, description, pictureUrl) {
+function createCard(name, description, pictureUrl, starts, ends) {
   return `
-    <div class="card">
+    <div class="card" class="shadow p-3 mb-5 bg-body rounded">
       <img src="${pictureUrl}" class="card-img-top">
       <div class="card-body">
         <h5 class="card-title">${name}</h5>
         <p class="card-text">${description}</p>
+      </div>
+      <div class="card-footer">
+        ${starts} - ${ends}
       </div>
     </div>
   `;
@@ -21,17 +24,25 @@ window.addEventListener("DOMContentLoaded", async () => {
     } else {
       const data = await response.json();
 
+      let index = 0;
       for (let conference of data.conferences) {
         const detailUrl = `http://localhost:8000${conference.href}`;
         const detailResponse = await fetch(detailUrl);
         if (detailResponse.ok) {
           const details = await detailResponse.json();
           const name = details.conference.name;
+          const location = details.conference.location.name;
           const description = details.conference.description;
           const pictureUrl = details.conference.location.picture_url;
-          const html = createCard(name, description, pictureUrl);
-          const column = document.querySelector(".col");
+          const starts = new Date(
+            details.conference.starts
+          ).toLocaleDateString();
+          const ends = new Date(details.conference.ends).toLocaleDateString();
+          const html = createCard(name, description, pictureUrl, starts, ends);
+          // const column = document.querySelector(".col");
+          const column = document.querySelector(`#col-${index % 3}`);
           column.innerHTML += html;
+          index += 1;
         }
       }
     }
