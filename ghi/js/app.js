e4434adbe0513960757a2037c10951c634234761
Qingying Meng <qmeng222@gmail.com>
2022-08-31 03:28:20 -0700

    error alert at the page

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,6 +1,6 @@
 function createCard(name, description, pictureUrl, starts, ends, location) {
   return `
-    <div class="card" class="shadow p-3 mb-5 bg-body rounded">
+    <div class="card shadow p-3 mb-4 bg-body rounded">
       <img src="${pictureUrl}" class="card-img-top">
       <div class="card-body">
         <h5 class="card-title">${name}</h5>
@@ -14,6 +14,14 @@ function createCard(name, description, pictureUrl, starts, ends, location) {
   `;
 }
 
+function createAlert() {
+  return `
+    <div class="alert alert-danger" role="alert">
+      Error alert — check it out!
+    </div>
+  `;
+}
+
 window.addEventListener("DOMContentLoaded", async () => {
   const url = "http://localhost:8000/api/conferences/";
 
@@ -21,7 +29,9 @@ window.addEventListener("DOMContentLoaded", async () => {
     const response = await fetch(url);
 
     if (!response.ok) {
-      // Figure out what to do when the response is bad
+      // Figure out what to do when the response is bad:
+      const alertTag = document.querySelector(".alert");
+      alertTag.innerHTML = createAlert();
     } else {
       const data = await response.json();
 
@@ -56,6 +66,8 @@ window.addEventListener("DOMContentLoaded", async () => {
     }
   } catch (e) {
     // Figure out what to do if an error is raised
+    const alertTag = document.querySelector(".alert");
+    alertTag.innerHTML = createAlert();
     console.error(e);
   }
 });
