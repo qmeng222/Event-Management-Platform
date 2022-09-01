0b517cca9ca36ae7ad8e85af84327e3181c1bf8f
Qingying Meng <qmeng222@gmail.com>
2022-08-31 20:33:48 -0700

    create new location POST request done

--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -3,7 +3,6 @@ window.addEventListener("DOMContentLoaded", async () => {
   const response = await fetch(url);
   if (response.ok) {
     const data = await response.json();
-    // console.log("data:", data);
     const selectTag = document.getElementById("state");
     for (let state of data.states) {
       const option = document.createElement("option");
@@ -12,4 +11,25 @@ window.addEventListener("DOMContentLoaded", async () => {
       selectTag.appendChild(option);
     }
   }
+  const formTag = document.getElementById("create-location-form");
+  formTag.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(formTag);
+    // Object.fromEntries() method takes a list of key-value pairs and returns a new object whose properties are given by those entries:
+    const json = JSON.stringify(Object.fromEntries(formData));
+
+    const locationUrl = "http://localhost:8000/api/locations/";
+    const fetchConfig = {
+      method: "post",
+      body: json,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(locationUrl, fetchConfig);
+    if (response.ok) {
+      formTag.reset();
+      const newLocation = await response.json();
+    }
+  });
 });
